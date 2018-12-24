f709a856dd49fcc2f38f9fc28d2dad492b6cb52d
StefanGolubovic <Stefan.golubovic3@gmail.com>
2018-12-24 18:57:04 +0100

    implementirana kolizija i nacrtan covek

--- src/main.c
+++ src/main.c
@@ -8,9 +8,9 @@
 #define timer_id2 1
 #define PI 3.14
 
-
 static int x_coord = 3;
 static float y_coord = 1;
+static float z_coord = 1;
 static int possible_x[] = {1,2,3,4,5};
 static int possible_obs[] = {0,1,2};
 static int timer_active;
@@ -22,6 +22,7 @@ static float x_planeB = 3;
 static float y_planeB = 0;
 static float z_planeB = 60;
 static int game_start = 0;
+static float angle_leg = 0;
 
 typedef struct{
     float x,y,z;
@@ -36,9 +37,14 @@ static int size_B = 0;
 static void on_display(void);
 static void on_reshape(int width, int height);
 static void on_keyboard(unsigned char key, int x, int y);
+
 static void set_planeA();
 static void set_planeB();
 static void move_planes(int value);
+static void check_colision();
+static float distance(Prepreka p);
+static void draw_man();
+
 static void jump(int value);
 static float jump_positions[180];
 static int jump_count = 0;
@@ -61,8 +67,8 @@ int main(int argc, char **argv) {
     timer_jump = 0;
     int k = 0;
     for(float i = 0;i <= 18;i+=0.1){
-        jump_positions[k] = 1.5*sin(i);
-        printf("jump_possition: %lf\n",jump_positions[k++]+1);
+        jump_positions[k++] = 1.5*sin(i);
+        // printf("jump_possition: %lf\n",jump_positions[k++]+1);
     }
     k = 0;
 
@@ -73,23 +79,11 @@ int main(int argc, char **argv) {
 
     /*OpenGL init*/
     glClearColor(0.7, 0.7, 0.7, 0);
-            glEnable(GL_DEPTH_TEST);
+    glEnable(GL_DEPTH_TEST);
 
-    /*enter main loop*/
-    glutMainLoop();
-
-    return 0;
-}
-
-static void on_display(void)
-{
-    
     GLfloat light_position[] = { 0, 5, 0, 0};
-    
-    GLfloat light_ambient[] = { 0.2, 0.2, 0.2, 1 };
-    
+    GLfloat light_ambient[] = { 0.1, 0.1, 0.1, 1 };
     GLfloat light_diffuse[] = { 0.7, 0.7, 0.7, 1 };
-    
     GLfloat light_specular[] = { 0.1, 0.1, 0.1, 1 };
 	
     /*ukljucuje se osvjetljenje i podesavaju parametri svetla*/
@@ -100,18 +94,27 @@ static void on_display(void)
     glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
     glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
 
-    /*zemljani*/
-    float mat_ambient[] ={ 0.19125f, 0.0735f, 0.0225f, 1.0f };
-    float mat_diffuse[] ={0.7038f, 0.27048f, 0.0828f, 1.0f };
-    float mat_specular[] ={0.256777f, 0.137622f, 0.086014f, 1.0f };
-    float shine[] = {12.8};
-    
-    /*zeleni*/
-    float mat_ambient1[] ={ 0.0f,0.05f,0.0f,1.0f };
-    float mat_diffuse1[] ={ 0.4f,0.5f,0.4f,1.0f};
-    float mat_specular1[] ={0.04f,0.7f,0.04f,1.0f };
-    float shine1[] =  {10.0f};
+    glLightModeli(GL_LIGHT_MODEL_TWO_SIDE,1);
+
+    /*enter main loop*/
+    glutMainLoop();
+
+    return 0;
+}
+
+static void on_display(void)
+{
+    float mat_ambient[] ={ 0.1,0.1,0.1,1};
+    float mat_diffuse_gray[] ={ .8,.8,.8,1};
+    float mat_diffuse_red[] ={ 0.7,0.1,0.1,1};
+    float mat_diffuse_yellow[] ={ 0.8,0.8,0.1,1};
+    float mat_specular[] ={0.2,0.2,0.2,1};
+    float shininess = 80;
         
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse_gray);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
 
     /* Brise se prethodni sadrzaj prozora. */
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -119,34 +122,32 @@ static void on_display(void)
     /* Podesava se vidna tacka. */
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(x_coord,7.0,-5.0,x_coord,5.0,0.0,0.0,1.0,0.0);
-
+    gluLookAt(x_coord,5.0,-7.0,
+                x_coord,3.0,0.0,
+                0.0,1.0,0.0);
 
     /* Kreira se objekat. */
+    // glPushMatrix();
+    //     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse_red);
+    //     glTranslatef(x_coord, y_coord, z_coord);
+    //     // draw_man();
+    //     glutSolidSphere(.5,40,40);
+    // glPopMatrix();
+
     glPushMatrix();
-        glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
-        glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-        glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-        glMaterialfv(GL_FRONT, GL_SHININESS, shine);
-        glTranslatef(x_coord, y_coord, 0);
-        glutSolidSphere(.5,40,40);
+        glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse_red);
+        draw_man();
     glPopMatrix();
     
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse_gray);
+
     glPushMatrix();
-        glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
-        glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-        glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-        glMaterialfv(GL_FRONT, GL_SHININESS, shine);
         glTranslatef(x_planeA,y_planeA,z_planeA); 
         glScalef(6,1,30);
         glutSolidCube(1);
     glPopMatrix();
 
     glPushMatrix();
-        glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient1);
-        glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse1);
-        glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular1);
-        glMaterialfv(GL_FRONT, GL_SHININESS, shine1);
         glTranslatef(x_planeB,y_planeB,z_planeB); 
         glScalef(6,1,30);
         glutSolidCube(1);
@@ -159,35 +160,21 @@ static void on_display(void)
     
     for(int i = 0; i < size_A ; i++){
         glPushMatrix();
-            glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient1);
-            glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse1);
-            glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular1);
-            glMaterialfv(GL_FRONT, GL_SHININESS, shine1);
+            glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse_yellow);
             glTranslatef(array_A[i].x,1,array_A[i].z);
-            switch(array_A[i].obst_type){
-            case 0: glutSolidCube(1);break;
-            case 1: glutSolidCone(.5,1,40,40);break;
-            case 2: glutSolidSphere(.5,40,40);break;
-        }
+            glutSolidCube(1);
         glPopMatrix();
     }
 
     for(int i = 0; i < size_B ; i++){
         glPushMatrix();
-            glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient1);
-            glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse1);
-            glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular1);
-            glMaterialfv(GL_FRONT, GL_SHININESS, shine1);
+            glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse_yellow);
             glTranslatef(array_B[i].x,1,array_B[i].z);
-            switch(array_A[i].obst_type){
-            case 0: glutSolidCube(1);break;
-            case 1: glutSolidCone(.5,1,40,40);break;
-            case 2: glutSolidSphere(.5,40,40);break;
-        }
+            glutSolidCube(1);
         glPopMatrix();
     }
 
-
+    glDisable(GL_LIGHTING);
     glBegin(GL_LINES);
         glColor3f(1,0,0);
         glVertex3f(0,0,0);
@@ -202,6 +189,7 @@ static void on_display(void)
         glVertex3f(0,0,100);
             
     glEnd();
+    glEnable(GL_LIGHTING);
         
     /* Nova slika se salje na ekran. */
     glutSwapBuffers();
@@ -215,9 +203,9 @@ static void on_reshape(int width, int height)
     /* Podesava se projekcija. */
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    /*gluPerspective(45, (float) width / height, 1, 40);*/
-    glFrustum (-1.0, 1.0, -1.0, 1.0, 1.0, 100.0);
-    glMatrixMode (GL_MODELVIEW);
+    gluPerspective(60, (float) width / height, .1, 30);
+    // glFrustum (-1.0, 1.0, -1.0, 1.0, 1.0, 100.0);
+    // glMatrixMode (GL_MODELVIEW);
 }
 
 static void on_keyboard(unsigned char key, int x, int y)
@@ -270,13 +258,18 @@ static void move_planes(int value)
     if(value)
         return;
 
+    angle_leg +=1;
+    if(angle_leg > 360)
+        angle_leg = 0;
     z_planeA -= 0.2;
     z_planeB -= 0.2;
     for(int i = 0 ; i < size_A ; i++){
         array_A[i].z -= 0.2;
+        check_colision();
     }
     for(int i = 0 ; i < size_B ; i++){
         array_B[i].z -= 0.2;
+        check_colision();
     }
     if(z_planeA + 15 < 0){
         z_planeA = 45;
@@ -290,7 +283,7 @@ static void move_planes(int value)
 
     glutPostRedisplay();
     if(timer_active) {
-        glutTimerFunc(50,move_planes,0);
+        glutTimerFunc(30,move_planes,0);
     }
 }
 
@@ -338,11 +331,92 @@ static void jump(int value)
     y_coord = 1 + jump_positions[jump_count++];
     // printf("Y coord : %lf",y_coord);
     if(jump_count < 32)
-        glutTimerFunc(30,jump,timer_id2);
+        glutTimerFunc(20,jump,timer_id2);
     else{
         timer_jump = 0;
         y_coord = 1;
         jump_count = 0;
     }
 }
+int k = 0;
+static void check_colision()
+{
+    if (z_planeA < z_planeB){
+        for(int i = 0 ; i < size_A ; i++){
+            // printf("udaljenost: %f",distance(array_A[i]));
+            //hardkodovano zbog greske u racunu
+            if (distance(array_A[i]) <= 1.02){
+                printf("Udario sam %d\n", k);
+                k++;
+            }
+        }
+    }
+    else{
+        for(int i = 0 ; i < size_B ; i++){
+            //hardkodovano zbog greske u racunu
+            if (distance(array_B[i]) <= 1.02){
+                printf("Udario sam %d\n", k);
+                k++;
+            }
+        }
 
+    }
+}
+
+static float distance(Prepreka p)
+{
+    float x = powf((p.x - x_coord),2);
+    float y = powf((p.y - y_coord),2);
+    float z = powf((p.z - z_coord),2);
+
+    return sqrtf(x + y + z);
+}
+
+static void draw_man()
+{
+    glPushMatrix();
+        glTranslatef(x_coord,y_coord+1.2,0);
+        glutSolidSphere(0.2,10,10);
+    glPopMatrix();
+    glPushMatrix();
+        glTranslatef(x_coord,y_coord+.6,0);
+        glScalef(.5,.7,.3);
+        glutSolidCube(1);
+    glPopMatrix();
+    glPushMatrix();
+        glTranslatef(x_coord+.15,y_coord+.1,0);
+        glScalef(.3,1.5,.3);
+        glPushMatrix();
+            glRotatef(20*cos(angle_leg*PI/15),1,0,0);
+            glutSolidCube(.5);
+        glPopMatrix();
+    glPopMatrix();
+    glPushMatrix();
+        glTranslatef(x_coord-.15,y_coord+.1,0);
+        //TODO stampaj MODELVIEW
+        glScalef(.3,1.5,.1);
+        glPushMatrix();
+            glRotatef(20*sin(angle_leg*PI/15),1,0,0);
+            glutSolidCube(.5);
+        glPopMatrix();
+    glPopMatrix();
+    glPushMatrix();
+        glTranslatef(x_coord+.5,y_coord-1.5,5);
+        glRotatef(40,0,0,1);
+        glScalef(.1,.6,.05);
+        glPushMatrix();
+            glRotatef(20*cos(angle_leg*PI/15),1,0,0);
+            glutSolidCube(1);
+        glPopMatrix();
+    glPopMatrix();
+    glPushMatrix();
+        glTranslatef(x_coord-.5,y_coord-1.5,5);
+
+        glRotatef(-40,0,0,1);
+        glScalef(.1,.6,.05);
+        glPushMatrix();
+            glRotatef(20*sin(angle_leg*PI/15),1,0,0);
+            glutSolidCube(1);
+        glPopMatrix();
+    glPopMatrix();
+}
